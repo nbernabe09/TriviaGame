1adec60bf23cec71d8a04c81708d6a9ce4f3102b
Nathan Bernabe <nbernabe09@aol.com>
2017-07-25 11:15:18 -0700

    Added more questions and small style changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
 	var questions = [
 		"What year did Fantasmic! open?",
 		"Which was the last attraction whose construction was overseen by Walt Disney?",
-		"What attraction was NOT available on opening day?"];
-	var answers = ["","",""]
+		"What attraction was NOT available on opening day?",
+		"Which is NOT one of the names of the Haunted Mansion's hitchhiking ghosts?",
+		"Holidayland was a recreation area with a separate entrance before being replaced by New Orleans Square.",
+		"Disney's FastPass system was first introduced in 2001 with the opening of Disney's California Adventure Park.",
+		"All of these attractions were replaced and rethemed, except:",
+		"Davy Crockett's Explorer Canoes used to be called Indian War Canoes."];
+	var answers = [];
 	answers[0] = [
 		"1990",
 		"1991",
@@ -17,10 +22,29 @@ $(document).ready(function() {
 		"Pirates of the Caribbean",
 		"Haunted Mansion"];
 	answers[2] = [
-	"Autopia",
-	"Casey Jr. Circus Train",
-	"Jungle Cruise",
-	"Peter Pan's Flight"];
+		"Autopia",
+		"Dumbo the Flying Elephant",
+		"Jungle Cruise",
+		"Peter Pan's Flight"];
+	answers[3] = [
+		"Ezra",
+		"Phineas",
+		"Gus",
+		"Victor"];
+	answers[4] = [
+		"True",
+		"False"];
+	answers[5] = [
+		"True",
+		"False"];
+	answers[6] = [
+		"Maliboomer",
+		"Orange Stinger",
+		"Mulholland Madness",
+		"Sun Wheel"];
+	answers[7] = [
+		"True",
+		"False"];
 	var time, ask, countdown, right, wrong, tooLong;
 	var clockRunning = false;
 
@@ -29,17 +53,17 @@ $(document).ready(function() {
 		right = 0;
 		wrong = 0;
 		tooLong = 0;
-		ask = 0;
+		ask = 7;
 		newQuestion(ask);
 	}
 	function newQuestion(x) {
 		if (ask === questions.length) {
 			$("#question").html("<div>Right Answers: " + right + "</div><div>Wrong Answers: " + wrong + "</div><div>Timeouts: " + tooLong + "</div>");
-			$("#answers").html("<div class=' btn btn-primary' id='replay'>Replay</div>");
+			$("#answers").html("<div class=' btn btn-primary btn-lg' id='replay'>Replay</div>");
 		} else {
 			$("#time").html("Time Remaining: <span id='limit'></span>");
 			$("#answers").html("");
-			time = 10;
+			time = 30;
 			$("#limit").html(time);
 			startGameTime();
 			$("#question").html(questions[x]);
@@ -48,7 +72,7 @@ $(document).ready(function() {
 	}
 	function listAnswers(x) {
 		for (var i = 0; i < (answers[x]).length; i++) {
-			$("#answers").append("<div class='row'><div class=' btn btn-primary' id='answer" + i + "'>" + answers[x][i] + "</div></div>");
+			$("#answers").append("<div class='row'><div class=' btn btn-primary btn-lg' id='answer" + i + "'>" + answers[x][i] + "</div></div>");
 		}
 	}
 	function rightPick() {
@@ -116,7 +140,7 @@ $(document).ready(function() {
 
 	// Button Clicks
 	$("body").on("click", "#answer0", function() {
-		if (ask === -1) {
+		if ((ask === 4) || (ask === 6) || (ask === 7)) {
 			stopTime();
 			rightPick();
 		} else {
@@ -125,7 +149,7 @@ $(document).ready(function() {
 		}
 	});
 	$("body").on("click", "#answer1", function() {
-		if (ask === 2) {
+		if ((ask === 2) || (ask === 5)) {
 			stopTime();
 			rightPick();
 		} else {
@@ -143,7 +167,7 @@ $(document).ready(function() {
 		}
 	});
 	$("body").on("click", "#answer3", function() {
-		if (ask === -1) {
+		if (ask === 3) {
 			stopTime();
 			rightPick();
 		} else {
